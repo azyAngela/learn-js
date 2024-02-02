448daf316920e1e8439c500b287c637643874d01
azyAngela <114364515+azyAngela@users.noreply.github.com>
2024-02-02 16:43:09 -0500

    quiz

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -10,8 +10,9 @@ function createTRNode(colNodes) {
   return trNode;
 }
 
-function createTDNode(childNode) {
+function createTDNode(childNode, id) {
   let tdNode = document.createElement("td");
+  tdNode.id = id;
   tdNode.appendChild(childNode);
   return tdNode;
 }
@@ -21,11 +22,33 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createBtnNode(txt, index="") {
+  let btnNode = document.createElement("button");
+  btnNode.innerText = txt;
+  btnNode.onclick = () => {
+    handleEditTextClick(index);
+  }
+  return btnNode;
+  // btnNode.appendChild(createTxtNode(txt));
+  // btnNode.id = id;
+  // return btnNode;
+}
+
+function handleEditTextClick(index) {
+  // let inputNode = document.getElementById(id);
+  // inputNode.disabled = false;
+  // inputNode.focus();
+  let cell = document.getElementById(`col - ${index}`);
+  cell.innerHTML = '<input type="text" id="input" value="Hello">';
+  
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
-    let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"), `col - ${i}`);
+    let col2 = createTDNode(createBtnNode("Edit", "cell" + i + "0"));
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
